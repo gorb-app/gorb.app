044b7142958fe074d054942adf11a9e9722322ec
JustTemmie <47639983+JustTemmie@users.noreply.github.com>
2025-05-02 02:57:46 +0200

    feat: include data for other platforms to generate embeds

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,19 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxt/image']
+  modules: ['@nuxt/fonts', '@nuxt/image'],
+  app: {
+    head: {
+      title: 'Gorb - The Chat App',
+      // this is purely used to embed in that other chat app, and similar stuff
+      meta: [
+        { property: 'og:title', content: 'Gorb - The Federated Chat App' },
+        { property: 'og:description', content: 'GORB! The open-source and federated chat application!' },
+        // { property: 'og:image', content: 'missing' },
+        { property: 'og:url', content: 'https://gorb.app' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:site_name', content: 'Gorb' }
+      ]
+    }
+  }
 })
