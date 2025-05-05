1e261d1a1bf35325e74065d52c8198faf91eed00
SauceyRed <saucey@saucey.red>
2025-05-05 11:18:09 +0200

    feat: set custom title and description for homepage

--- pages/index.vue
+++ pages/index.vue
@@ -15,6 +15,13 @@
 
 <script lang="ts" setup>
 
+useHead({
+  title: "Gorb",
+  meta: [
+    { name: "description", content: "Welcome to Gorb, the Open-Source Chat App!" }
+  ]
+});
+
 </script>
 
 <style scoped>
